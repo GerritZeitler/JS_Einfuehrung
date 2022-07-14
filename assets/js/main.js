12f8b73330d281828065ffa3ee93d8191d3adf73
supercode <getlucked@gmx.de>
2022-07-14 14:07:32 +0200

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,129 @@
+/*  EINFÜHRUNG CONSOLE LOG */
+/* a = 12;
+b = a * 3;
+console.log(b);
+
+console.log(document.URL);
+
+document.write(b)
+
+console.log("Hello World")
+
+console.log("Anton")
+
+let firstName = ("Anton")
+
+console.log(firstName)
+
+let age = ("28")
+
+console.log(age)
+
+let job = ("Trainer")
+
+console.log(job)
+
+let married = (true)
+
+console.log(married)
+
+console.log(firstName + " ist " + age + " alt, von Beruf " + job + " und ist verheiratet: " + married) */
+
+/* EINFÜHRUNG VARIABLEN */
+
+/* let carName = ("BMW")
+
+let x = (150)
+console.log(x)
+let y = (50)
+console.log(y)
+let z = (x + y)
+console.log(z)
+
+let firstName = "John", lastName = "Doe", age = "35";
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+ */
+
+/* EINFÜRHUNG ARITHMETIC */
+
+/* let score = 5 + 5 * 10
+console.log('Ergebnis: ' + score); */
+
+/* let score = (5 + 5) * 10;
+console.log('Ergebnis: ' + score); */
+
+/* let score = 0;
+score = score + 10;
+console.log('Ergebnis: ' + score); */
+/* 
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+
+zahl++;
+console.log('increment: ' + zahl);
+
+zahl--;
+console.log('dekrement: ' + zahl);
+ */
+
+
+/* EINFÜHRUNG DOCUMENT WRITE */
+
+/* document.write("<h1> Hello World</h1>")
+
+document.write("<p>Have a nice day!</p>")
+
+let myText = "Hello again"
+
+document.write(myText)
+
+let firstName = "Gerrit"
+let lastName = "Zeitler"
+document.write("<br>");
+document.write("<br>");
+document.write("Vorname: " + firstName)
+document.write("<br>");
+document.write("Nachname: " + lastName) */
+
+
+/* EINFÜHRUNG STRING METHODS */
+
+/* let x = "Hello "
+let y = "World<br>"
+
+let z = x + y
+document.write(z)
+
+let n = x + " " + y
+document.write(n)
+
+document.write(x + "World") nicht 100% verstanden
+ */
+
+/* let meinString = "Ich bin Erster ";
+let ichLetzter = "Ich komme auf Platz zwei";
+
+meinString += ichLetzter
+
+console.log(meinString)
+ */
+
+/* EINFÜHRUNG WINDOW OBJECT METHOD */
+
+
+/* let alter = prompt("Gib mir bitte dein Alter:");
+console.log(alter);
+ */
+
+/* let b = 5;
+let a = b * 5 - 3;
+alert(a);
+ */
+
+/* let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+alert(mann + grosse); */
